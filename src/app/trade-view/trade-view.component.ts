f1dd848bcf446027e00dae10f461a70c71889e58
Laredo Tirnanic <LaredoT@webuycars.co.za>
2020-02-23 12:50:10 +0200

    Started styling

--- src/app/trade-view/trade-view.component.ts
+++ src/app/trade-view/trade-view.component.ts
@@ -10,10 +10,10 @@ import * as moment from "moment";
 import { PlotlyModule } from "angular-plotly.js";
 import { EMA, MACD, CCI } from "technicalindicators";
 import { EmaResult } from "./models/ema-result";
-import { MACDInput } from 'technicalindicators/declarations/moving_averages/MACD';
-import { MACDResult } from './models/macd-result';
-import { CCIInput } from 'technicalindicators/declarations/oscillators/CCI';
-import { CCIResult } from './models/cci-result';
+import { MACDInput } from "technicalindicators/declarations/moving_averages/MACD";
+import { MACDResult } from "./models/macd-result";
+import { CCIInput } from "technicalindicators/declarations/oscillators/CCI";
+import { CCIResult } from "./models/cci-result";
 
 @Component({
   selector: "app-trade-view",
@@ -30,7 +30,8 @@ export class TradeViewComponent implements OnInit, AfterViewInit {
   }
 
   startDate = moment("2019-01-01", "YYYY-MM-DD HH:mm:ss");
-  endDate = moment("2020-02-01", "YYYY-MM-DD HH:mm:ss");
+  endDate = moment("2020-03-01", "YYYY-MM-DD HH:mm:ss");
+  volumeColors = [];
 
   public candleGraph = {
     config: {
@@ -56,13 +57,21 @@ export class TradeViewComponent implements OnInit, AfterViewInit {
         x: [],
         y: [],
         type: "lines",
-        name: "50"
+        name: "50",
+        line: {
+          color: "#07570d",
+          // width: 1
+        }
       },
       {
         x: [],
         y: [],
         type: "lines",
-        name: "100"
+        name: "100",
+        line: {
+          color: "#080aa0",
+          // width: 1
+        }
       },
       {
         x: [],
@@ -70,12 +79,15 @@ export class TradeViewComponent implements OnInit, AfterViewInit {
         type: "lines",
         name: "200",
         xaxis: "x",
-        showlegend: "False"
+        line: {
+          color: "#ad080c",
+          // width: 1
+        }
       },
       {
         x: [],
         y: [],
-        legendgroup: 'MACD',
+        legendgroup: "MACD",
         showlegend: true,
         type: "lines",
         name: "MACD",
@@ -85,7 +97,7 @@ export class TradeViewComponent implements OnInit, AfterViewInit {
       {
         x: [],
         y: [],
-        legendgroup: 'MACD',
+        legendgroup: "MACD",
         showlegend: false,
         type: "bar",
         name: "Hist",
@@ -95,7 +107,7 @@ export class TradeViewComponent implements OnInit, AfterViewInit {
       {
         x: [],
         y: [],
-        legendgroup: 'MACD',
+        legendgroup: "MACD",
         showlegend: false,
         type: "lines",
         name: "Signal",
@@ -105,7 +117,7 @@ export class TradeViewComponent implements OnInit, AfterViewInit {
       {
         x: [],
         y: [],
-        legendgroup: 'CCI',
+        legendgroup: "CCI",
         showlegend: true,
         type: "lines",
         name: "CCI",
@@ -115,7 +127,7 @@ export class TradeViewComponent implements OnInit, AfterViewInit {
       {
         x: [],
         y: [],
-        legendgroup: 'CCI',
+        legendgroup: "CCI",
         showlegend: false,
         type: "lines",
         name: "CCI100",
@@ -125,19 +137,36 @@ export class TradeViewComponent implements OnInit, AfterViewInit {
       {
         x: [],
         y: [],
-        legendgroup: 'CCI',
+        legendgroup: "CCI",
         showlegend: false,
         type: "lines",
         name: "CCI-100",
         xaxis: "x",
         yaxis: "y3"
+      },
+      {
+        x: [],
+        y: [],
+        showlegend: false,
+        type: "bar",
+        name: "Volume",
+        xaxis: "x",
+        // yaxis: "y4"
+        mode: "markers",
+        marker: {
+          color: this.volumeColors,
+          // symbol: symbolForThresholds,
+          size: 5
+        }
       }
     ],
     layout: {
+      plot_bgcolor: "#131722",
+      paper_bgcolor: "#131722",
       grid: {
-        rows: 3,
+        rows: 4,
         columns: 1,
-        subplots: [["xy"], ["xy2"], ["xy3"]],
+        subplots: [["xy"], ["xy2"], ["xy3"], ["xy4"]],
         roworder: "top to bottom"
       },
       autosize: true,
@@ -152,9 +181,16 @@ export class TradeViewComponent implements OnInit, AfterViewInit {
       },
       showlegend: true,
       legend: {
-        "orientation": "h",
-        x: 0.4, 
-        y: 1.2
+        orientation: "h",
+        x: 0.025,
+        y: 1,
+        xanchor: "left",
+        font: {
+          // family: "sans-serif",
+          size: 12,
+          color: "#92969e"
+        }
+        // bgcolor: "#92969e"
       },
       xaxis: {
         autorange: true,
@@ -207,19 +243,87 @@ export class TradeViewComponent implements OnInit, AfterViewInit {
             this.endDate.utc().format("YYYY-MM-DD HH:mm:ss")
           ]
         },
-        type: "date"
+        type: "date",
+        tickcolor: "#787878",
+        linecolor: "#787878",
+        gridcolor: "#363c4e",
+        titlefont: {
+          family: "Arial, sans-serif",
+          size: 18,
+          color: "#cccdcd"
+        },
+        tickfont: {
+          family: "Old Standard TT, serif",
+          size: 14,
+          color: "#cccdcd"
+        }
       },
       yaxis: {
         autorange: true,
-        domain: [0.4, 1],
-        range: [2000, 20000],
-        type: "linear"
+        domain: [0.55, 1],
+        range: [5000, 14000],
+        type: "linear",
+        tickcolor: "#787878",
+        linecolor: "#787878",
+        gridcolor: "#363c4e",
+        titlefont: {
+          // family: "Arial, sans-serif",
+          size: 18,
+          color: "#cccdcd"
+        },
+        tickfont: {
+          family: "Old Standard TT, serif",
+          size: 14,
+          color: "#cccdcd"
+        }
       },
       yaxis2: {
-        domain: [0.2, 0.3] 
+        domain: [0.35, 0.55],
+        tickcolor: "#787878",
+        linecolor: "#787878",
+        gridcolor: "#363c4e",
+        titlefont: {
+          // family: "Arial, sans-serif",
+          size: 18,
+          color: "#cccdcd"
+        },
+        tickfont: {
+          family: "Old Standard TT, serif",
+          size: 14,
+          color: "#cccdcd"
+        }
       },
       yaxis3: {
-        domain: [0, 0.1] 
+        domain: [0.1, 0.35],
+        tickcolor: "#787878",
+        linecolor: "#787878",
+        gridcolor: "#363c4e",
+        titlefont: {
+          // family: "Arial, sans-serif",
+          size: 18,
+          color: "#cccdcd"
+        },
+        tickfont: {
+          family: "Old Standard TT, serif",
+          size: 14,
+          color: "#cccdcd"
+        }
+      },
+      yaxis4: {
+        domain: [0, 0.1],
+        tickcolor: "#787878",
+        linecolor: "#787878",
+        gridcolor: "#363c4e",
+        titlefont: {
+          // family: "Arial, sans-serif",
+          size: 18,
+          color: "#cccdcd"
+        },
+        tickfont: {
+          family: "Old Standard TT, serif",
+          size: 14,
+          color: "#cccdcd"
+        }
       },
       annotations: [
         {
@@ -261,9 +365,13 @@ export class TradeViewComponent implements OnInit, AfterViewInit {
       .returnTrades(
         this.startDate.utc().unix(),
         this.endDate.utc().unix(),
-        14400
+        86400
       )
       .toPromise();
+
+    let volume = [];
+
+    let previousVolume = 0;
 
     results.forEach(data => {
       let date = moment
@@ -275,6 +383,15 @@ export class TradeViewComponent implements OnInit, AfterViewInit {
       this.candleGraph.data[0].high.push(data.high);
       this.candleGraph.data[0].low.push(data.low);
       this.candleGraph.data[0].open.push(data.open);
+      volume.push(data.volume / 4000);
+
+      if (previousVolume > data.volume) {
+        // Volume lower than previous
+        this.volumeColors.push("#ef5350");
+      } else {
+        this.volumeColors.push("#26a69a");
+      }
+      previousVolume = data.volume;
     });
 
     let ema50Result = this.addEmaChart(
@@ -304,15 +421,17 @@ export class TradeViewComponent implements OnInit, AfterViewInit {
     this.candleGraph.data[3].x = ema200Result.x;
     this.candleGraph.data[3].y = ema200Result.y;
 
-    let macdResult = this.addMACDChart({
-      values: this.candleGraph.data[0].close,
-      fastPeriod: 5,
-      slowPeriod: 8,
-      signalPeriod: 3,
-      SimpleMAOscillator: false,
-      SimpleMASignal: false
-    },
-    this.candleGraph.data[0].x)
+    let macdResult = this.addMACDChart(
+      {
+        values: this.candleGraph.data[0].close,
+        fastPeriod: 5,
+        slowPeriod: 8,
+        signalPeriod: 3,
+        SimpleMAOscillator: false,
+        SimpleMASignal: false
+      },
+      this.candleGraph.data[0].x
+    );
 
     this.candleGraph.data[4].x = macdResult.x;
     this.candleGraph.data[4].y = macdResult.MACD;
@@ -323,20 +442,33 @@ export class TradeViewComponent implements OnInit, AfterViewInit {
     this.candleGraph.data[6].x = macdResult.x;
     this.candleGraph.data[6].y = macdResult.signal;
 
-    let cciResult = this.addCCI({
-      high:this.candleGraph.data[0].high, 
-      low: this.candleGraph.data[0].low, 
-      close: this.candleGraph.data[0].close
-      , period:21}, this.candleGraph.data[0].x)
+    let cciResult = this.addCCI(
+      {
+        high: this.candleGraph.data[0].high,
+        low: this.candleGraph.data[0].low,
+        close: this.candleGraph.data[0].close,
+        period: 21
+      },
+      this.candleGraph.data[0].x
+    );
 
     this.candleGraph.data[7].x = cciResult.x;
     this.candleGraph.data[7].y = cciResult.y;
 
-    this.candleGraph.data[8].x = [this.candleGraph.data[0].x[0], this.candleGraph.data[0].x[this.candleGraph.data[0].x.length - 1]];
-    this.candleGraph.data[8].y = [100,100]; 
+    this.candleGraph.data[8].x = [
+      this.candleGraph.data[0].x[0],
+      this.candleGraph.data[0].x[this.candleGraph.data[0].x.length - 1]
+    ];
+    this.candleGraph.data[8].y = [100, 100];
 
-    this.candleGraph.data[9].x = [this.candleGraph.data[0].x[0], this.candleGraph.data[0].x[this.candleGraph.data[0].x.length - 1]];
-    this.candleGraph.data[9].y = [-100,-100]; 
+    this.candleGraph.data[9].x = [
+      this.candleGraph.data[0].x[0],
+      this.candleGraph.data[0].x[this.candleGraph.data[0].x.length - 1]
+    ];
+    this.candleGraph.data[9].y = [-100, -100];
+
+    this.candleGraph.data[10].x = this.candleGraph.data[0].x;
+    this.candleGraph.data[10].y = volume;
   }
 
   private addEmaChart(
@@ -360,11 +492,7 @@ export class TradeViewComponent implements OnInit, AfterViewInit {
     return result;
   }
 
-  private addMACDChart(
-    input: MACDInput,
-    xData: undefined[]
-  ) : MACDResult
-  {
+  private addMACDChart(input: MACDInput, xData: undefined[]): MACDResult {
     let result = new MACDResult();
 
     var macd = new MACD(input);
@@ -376,11 +504,10 @@ export class TradeViewComponent implements OnInit, AfterViewInit {
       result.signal.push(macdResult.signal);
     });
 
-    let indexDifference =  xData.length - macdResults.length; 
+    let indexDifference = xData.length - macdResults.length;
 
     for (let index = 0; index < xData.length; index++) {
-      if (index >= indexDifference)
-      {
+      if (index >= indexDifference) {
         result.x.push(xData[index]);
       }
     }
@@ -388,24 +515,19 @@ export class TradeViewComponent implements OnInit, AfterViewInit {
     return result;
   }
 
-  private addCCI(
-    input: CCIInput,
-    xData: undefined[]
-    ) : CCIResult
-  {
+  private addCCI(input: CCIInput, xData: undefined[]): CCIResult {
     let result = new CCIResult();
     let cci1 = new CCI(input);
     var cciResults = cci1.getResult();
 
     cciResults.forEach(cciResult => {
-      result.y.push(cciResult)
+      result.y.push(cciResult);
     });
 
-    let indexDifference =  xData.length - cciResults.length; 
+    let indexDifference = xData.length - cciResults.length;
 
     for (let index = 0; index < xData.length; index++) {
-      if (index >= indexDifference)
-      {
+      if (index >= indexDifference) {
         result.x.push(xData[index]);
       }
     }
